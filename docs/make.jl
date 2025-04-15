826aede53d08849018d1ceabc54cfc520b57267e
LisaSchlueter <lschlueter@lbl.gov>
2025-04-15 16:36:10 -0700

    add tutorials

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,25 @@
 using Juleanita
 using Documenter
+using Literate 
 
 DocMeta.setdocmeta!(
         Juleanita, 
         :DocTestSetup, 
         :(using Juleanita); 
         recursive=true)
+
+function fix_literate_output(content)
+    content = replace(content, "EditURL = \"@__REPO_ROOT_URL__/\"" => "")
+    return content
+end
+
+gen_content_dir = joinpath(@__DIR__, "src", "tutorials")
+for tut_lit_fn in filter(fn -> endswith(fn, "_lit.jl"), readdir(gen_content_dir))
+    lit_src_fn = joinpath(gen_content_dir, tut_lit_fn)
+    tut_basename = tut_lit_fn[1:end-7] # remove "_lit.jl"
+    Literate.notebook(lit_src_fn, gen_content_dir, name = tut_basename, documenter = true, credit = true, execute = false)
+    Literate.markdown(lit_src_fn, gen_content_dir, name = tut_basename, documenter = true, credit = true, postprocess = fix_literate_output)
+end
 
 makedocs(;
     modules=[Juleanita],
@@ -17,6 +31,10 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
+        "Tutorials" => [
+            "Getting Started" => "tutorials/getting_started.md",
+            "Basic I/O" => "tutorials/reading_data.md",
+        ],
         "API" => "api.md",
         "LICENSE" => "LICENSE.md",
     ],
